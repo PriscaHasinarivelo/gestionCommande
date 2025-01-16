8412f58db4810c58422d533d53b577060097198f
Prisca Hasinarivelo <priscahasinarivelo@gmail.com>
2025-01-16 15:31:23 +0300

    modification login

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -10,51 +10,13 @@ function NavigationBar() {
   }, [utilisateurConnecte]);
   return (
     <>
-      <label className="label_user_login">{utilisateurConnecte}</label>
+      <label>Connection or deconnection</label>
     </>
   );
 }
 
 function ComboLogin() {
-  return (
-    <>
-      <li className="nav-item dropdown">
-        <a
-          className="nav-link dropdown-toggle"
-          id="navbarDropdown"
-          href="#"
-          role="button"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          <i className="fas fa-user fa-fw"></i>
-        </a>
-        <ul
-          className="dropdown-menu dropdown-menu-end"
-          aria-labelledby="navbarDropdown"
-        >
-          <li>
-            <a className="dropdown-item" href="#!">
-              Settings
-            </a>
-          </li>
-          <li>
-            <a className="dropdown-item" href="#!">
-              Activity Log
-            </a>
-          </li>
-          <li>
-            <hr className="dropdown-divider" />
-          </li>
-          <li>
-            <a className="dropdown-item" href="#!">
-              Logout
-            </a>
-          </li>
-        </ul>
-      </li>
-    </>
-  );
+  return <></>;
 }
 
 export default NavigationBar;
